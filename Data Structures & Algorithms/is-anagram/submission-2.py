458855100550891eeb12e41155573257b802dcf9
kingkushalchebiyam <kushalchebiyam@g.ucla.edu>
2026-09-10 18:05:54 -0700

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        hashmap = {} #char -> number of appearances
+
+        if len(s) != len(t): 
+            return False
+
+        countS = defaultdict(int)
+        countT = defaultdict(int)
+
+        for i in range(len(s)): 
+            countS[s[i]] += 1
+            countT[t[i]] += 1
+        
+        return countS == countT
+
+
+
+      
+        
